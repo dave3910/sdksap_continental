eb61b0146daced1f5015663ce8bacc48b6afb879
David Mechato <jmechato@vsperu.com>
2025-10-20 12:42:43 -0500

    Filtrado de socios mediante query con recordset

---
+++ DIAPI/DIAPI/Connection/ConexionSAP.cs
@@ -0,0 +1,61 @@
+﻿using SAPbobsCOM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIAPI.Connection
+{
+    public class ConexionSAP
+    {
+        public string Server { get; set; }
+        public string DBName { get; set; }
+        public string DBUserName { get; set; }
+        public string DBPass { get; set; }
+        public string SAPUser { get; set; }
+        public string SAPPass { get; set; }
+
+        private Company _company;
+
+        public ConexionSAP(string server, string dbName,string dbUserName, string dbPass, string sapUser, string sapPass)
+        {
+            this.Server = server;
+            this.DBName = dbName;
+            this.DBPass = dbPass;
+            this.SAPUser = sapUser;
+            this.SAPPass = sapPass;
+            this.DBUserName = dbUserName;
+
+        }
+
+        public Company GetSAPConnection()
+        {
+            try
+            {
+                _company = new Company();
+                _company.Server = this.Server;
+                _company.CompanyDB = this.DBName;
+                _company.DbUserName = this.DBUserName;
+                _company.DbPassword = this.DBPass;
+                _company.UserName = this.SAPUser;
+                _company.Password = this.SAPPass;
+
+                _company.DbServerType = BoDataServerTypes.dst_HANADB;
+                _company.language = BoSuppLangs.ln_Spanish_La;
+
+                int ret = _company.Connect();
+                if (ret != 0)
+                    throw new Exception(_company.GetLastErrorDescription());
+
+                return _company;
+                
+            }
+            catch (Exception)
+            {
+                throw ;
+            }
+        }
+
+    }
+}
---
+++ DIAPI/DIAPI/Program.cs
@@ -0,0 +1,50 @@
+﻿using DIAPI.Connection;
+using SAPbobsCOM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SERVIDOR SAP (IP O HOSTNAME + PUERTO)
+            //TIPO DE BASE DE DATOS (SQL / HANA)
+            //EL NOMBRE DEL ESQUEMA DE ACCESO
+            //USUARIO DE BASE DE DATOS
+            //PASSWORD DE BASE DE DATOS
+            //USUARIO SAP
+            //PASSWORD SAP
+            //LANGUAJE
+
+            //OBTENEMOS CONEXIÓN
+            
+            string server = "NDB@192.168.1.14:30013";
+            string schema = "B1H_LAZZOS_PROD2503";
+            string userdb = "SYSTEM";
+            string passdb = "HANAB1Admin";
+            string usersap = "manager";
+            string passsap = "sbosap";
+
+            try
+            {
+                ConexionSAP con = new ConexionSAP(server, schema, userdb, passdb, usersap, passsap);
+                Company company = con.GetSAPConnection();
+
+                Console.WriteLine($"Conexión establecida correctamente con esquema {company.CompanyName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
